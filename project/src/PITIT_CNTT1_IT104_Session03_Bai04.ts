c22ed9dfe1fe792c654399fb4438935f782930f0
duc123694 <tienducle86@gmail.com>
2025-08-06 19:50:45 +0700

    sub

---
+++ project/src/PITIT_CNTT1_IT104_Session03_Bai04.ts
@@ -0,0 +1,6 @@
+const strvalue:string = "2";
+const numvalue:number = 2;
+//console.log(strvalue == numvalue);
+// == so sánh giá trị lỏng lẻo nên 2 của string đc ép kiểu thành 2 của number  => true
+//console.log(strvalue === numvalue);
+// === cái anyf là so sánh giá trị lẫn cả kiểu dữ liệu nên => false
