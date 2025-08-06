4160ce034f96f6577a31f2b60a78c05ec6d4fb91
duc123694 <tienducle86@gmail.com>
2025-08-06 19:48:48 +0700

    sub

--- project/dist/PITIT_CNTT1_IT104_Session03_Bai07.js
+++ project/dist/PITIT_CNTT1_IT104_Session03_Bai07.js
@@ -1,1 +1,13 @@
 "use strict";
+let string : string = "hello world";
+
+let newString = "";
+
+for (let i = 0; i < string.length; i++) {
+    if (newString.includes(string[i])) {
+        continue;
+    } else {
+        newString += string[i];
+    }
+}
+console.log("New string: ", newString);
