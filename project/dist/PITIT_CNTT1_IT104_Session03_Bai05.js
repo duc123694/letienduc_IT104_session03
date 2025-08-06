b002d39433926d371d4ff8b79e88413c2facd62b
duc123694 <tienducle86@gmail.com>
2025-08-06 19:48:10 +0700

    sub

--- project/dist/PITIT_CNTT1_IT104_Session03_Bai05.js
+++ project/dist/PITIT_CNTT1_IT104_Session03_Bai05.js
@@ -1,1 +1,18 @@
 "use strict";
+let firstName: string = "john";
+let lastName: string = "doe";
+const capitalize = (word: string): string => {
+  if (word.length === 0) {
+    return "";
+  }
+  const firstChar = word[0].toUpperCase();
+  const rest = word.slice(1);
+  return firstChar + rest;
+};
+
+firstName = capitalize(firstName);
+lastName = capitalize(lastName);
+
+const fullName: string = `${firstName} ${lastName}`;
+
+console.log(fullName);
